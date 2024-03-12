1bcd3ae67fe4dbc518ae2a0ffd67bd8e9cf0f0de
pigeatgarlic <huyhoangdo0205@gmail.com>
2024-03-12 16:18:01 +0700

    moonlight child process

--- tauri.ts
+++ tauri.ts
@@ -1,27 +1,37 @@
+import { Body, ResponseType, getClient } from '@tauri-apps/api/http';
 import { Child, Command } from "@tauri-apps/api/shell";
 
 
+const map = new Map<string, { child: Child, req: StartRequest }>()
+type StartRequest = { 
+    username: string, 
+    password: string, 
+    address: string, 
+    id: number, 
+}
 
-const map = new Map<string, Child>()
-let log_raw = ''
 
-type StartRequest = { username: number, password: number, address: string }
-export async function StartMoonlight({ username, password, address }: StartRequest): Promise<string> {
-    const command = new Command('Moonlight', [
+
+
+export async function StartMoonlight(req: StartRequest): Promise<string> {
+    const { username, password, address } = req
+    const cmds = [
         '--url',
         address,
         '--username',
-        username.toString(10),
+        username,
         '--password',
-        password.toString(10)
-    ]);
+        password
+    ]
+    console.log(`starting moonlight with ${cmds}`)
+    const command = new Command('Moonlight', cmds);
 
-    command.stderr.addListener('data', (data) => log_raw += data);
-    command.stdout.addListener('data', (data) => log_raw += data);
+    command.stderr.addListener('data', (data) => console.log(data));
+    command.stdout.addListener('data', (data) => console.log(data));
     const child = await command.spawn()
 
     const ret = crypto.randomUUID()
-    map.set(ret, child)
+    map.set(ret, { child, req })
 
     return ret;
 };
@@ -29,48 +39,71 @@ export async function StartMoonlight({ username, password, address }: StartReque
 
 
 export async function CloseMoonlight(uuid: string): Promise<Error | 'SUCCESS'> {
+    const client = await getClient();
     const child = map.get(uuid)
     if (child == undefined)
         return new Error('invalid uuid')
 
-    await child.kill()
+    await child.child.kill()
+    console.log('/close request ' + child.req.id)
+    await client.post(`http://${child.req.address}:60000/closed`, Body.json({
+        id: child.req.id
+    }), { responseType: ResponseType.Text });
+
     return 'SUCCESS'
 }
 
 
-export function TakeLog(): string[] {
-    const ret: string[] = []
-    log_raw.split('\n').forEach(x => x.split('\r').forEach(y => ret.push(y)))
-    log_raw = ''
-    return ret
-}
 
 
 export async function ConfigureDaemon(address: string): Promise<StartRequest> {
-    let resp = await fetch(`http://${address}/initialize`, {
-        method: 'POST',
-        body: JSON.stringify({})
-    });
+    const client = await getClient();
+    try {
+        await client.post(`http://${address}:60000/initialize`, Body.json({}), { responseType: ResponseType.Text });
+    } catch { }
 
+    const sessions = (await client.get(`http://${address}:60000/sessions`)).data as {
+        id: any
+    }[];
 
-    resp = await fetch(`http://${address}/info`);
-
-    const sunshine = {
-        username: getRandomInt(0, 9999),
-        password: getRandomInt(0, 9999)
+    for (let index = 0; index < sessions.length; index++) {
+        const element = sessions[index];
+        console.log('/close request ' + element.id)
+        await client.post(`http://${address}:60000/closed`, Body.json(element), { responseType: ResponseType.Text });
     }
 
-    resp = await fetch(`http://${address}/new`, {
-        method: 'POST',
-        body: JSON.stringify({
-            id: 0,
-            timestamp: new Date().toISOString(),
-            sunshine
-        })
-    });
 
 
-    return { ...sunshine, address }
+    const sunshine = {
+        username: getRandomInt(0, 9999).toString(),
+        password: getRandomInt(0, 9999).toString()
+    }
+
+    const id = getRandomInt(0, 100)
+    const resp = await client.post(`http://${address}:60000/new`, Body.json({
+        id,
+        timestamp: new Date().toISOString(),
+        sunshine,
+        display: {
+            ScreenWidth: 1920,
+            ScreenHeight: 1080,
+        }
+    }), { responseType: ResponseType.Text }
+    );
+
+
+
+    if (!resp.ok)
+        throw new Error(resp.data as string)
+    else
+        console.log('/new request return ' + resp.data)
+
+
+    return {
+        ...sunshine,
+        address,
+        id,
+    }
 }
 
 
@@ -78,4 +111,13 @@ function getRandomInt(min: number, max: number) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
+
+async function JoinZeroTier(network_id: string): Promise<string> {
+    const command = await new Command('ZeroTier', ['leave',network_id]).execute();
+    return command.stdout + '\n' + command.stderr
+}
+async function LeaveZeroTier(network_id: string) : Promise<string> {
+    const command = await new Command('ZeroTier', ['join',network_id]).execute();
+    return command.stdout + '\n' + command.stderr
 }
